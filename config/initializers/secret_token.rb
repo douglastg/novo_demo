73a84f6c9fa1168d5bfb923cfc6e79828797bf22
Douglas <douglas@libring.com>
2014-06-10 15:16:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NovoDemo::Application.config.secret_key_base = 'abe0210729caa518448b6ce28df89a4ea9f8ff1ff10818862888f7ee4280239c37cbac3b6ded29914ea9a83121ab6391a4c44441c51d9e5118d5a8d5e1e52c8b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NovoDemo::Application.config.session_store :cookie_store, key: '_novo_demo_session'
